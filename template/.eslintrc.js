225cebcba76514a9345a6fad9f71cf2b75d1a0a5
rei <romanrei.dev@gmail.com>
2018-02-04 00:07:25 +0300

    [ESLint integration]

---
+++ template/.eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    es6: true
+    // node: true
+  },
+  {{#if_eq eslint "standard"}}
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue
+    'plugin:vue/strongly-recommended', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  {{/if_eq}}
+
+  {{#if_eq eslint "none"}}
+  extends: ['plugin:vue/strongly-recommended'],
+  {{/if_eq}}
+  
+  plugins: [
+    
+  ],
+  // check if imports actually resolve
+  settings: {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.config.js'
+      }
+    }
+  },
+
+  rules: {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
