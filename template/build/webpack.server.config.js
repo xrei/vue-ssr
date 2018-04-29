451b8069c7f3e8edc193b31962c9e0801edd417e
xrei <romanrei.dev@gmail.com>
2018-04-29 20:50:20 +0300

    Upd config

--- template/build/webpack.server.config.js
+++ template/build/webpack.server.config.js
@@ -21,7 +21,7 @@ module.exports = merge(base, {
   // https://github.com/liady/webpack-node-externals
   externals: nodeExternals({
     // do not externalize CSS files in case we need to import it from a dep
-    whitelist: /\.css$/
+    whitelist: [/\.css$/, /\?vue&type=style/] // https://vue-loader.vuejs.org/migrating.html#ssr-externals v15
   }),
   plugins: [
     new webpack.DefinePlugin({
